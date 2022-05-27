34007486e3a856ecda1c5b252d5487b0a01f488a
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-05-27 15:44:08 +0800

    Calculator

---
+++ practical_01/MyApplication/MyApplication/Pages/Calculator.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApplication.Pages
+{
+    public class CalculatorModel : PageModel
+    {
+        [BindProperty]
+        public decimal Num1 { get; set; }
+
+        [BindProperty]
+        public decimal Num2 { get; set; }
+
+        [BindProperty]
+        public string? Operator { get; set; }
+
+        public List<string> OperatorList { get; set; } = new() { "+", "-", "x", "/" };
+
+        public string? ResultStr { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrEmpty(Operator))
+            {
+                ResultStr = string.Empty;
+            }
+            else if (Operator.Equals("+"))
+            {
+                ResultStr = Convert.ToString(Num1 + Num2);
+            }
+            else if (Operator.Equals("-"))
+            {
+                ResultStr = Convert.ToString(Num1 - Num2);
+            }
+            else if (Operator.Equals("x"))
+            {
+                ResultStr = Convert.ToString(Num1 * Num2);
+            }
+            else if (Operator.Equals("/"))
+            {
+                ResultStr = (Num2 == 0) ? "NA" : Convert.ToString(Num1 / Num2);
+            }
+            else
+            {
+                ResultStr = string.Empty;
+            }
+
+            return Page();
+        }
+    }
+}
