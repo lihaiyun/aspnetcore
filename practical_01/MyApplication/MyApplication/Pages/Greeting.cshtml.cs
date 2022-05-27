6152562ef9d6327e70d52f1ee24c89601fdc04c1
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-05-27 15:41:53 +0800

    My Application - Accounts, Greeting

---
+++ practical_01/MyApplication/MyApplication/Pages/Greeting.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApplication.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        [BindProperty]
+        public string? Name { get; set; }
+
+        public string? Greeting { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            Greeting = string.Format("Welcome {0}!", string.IsNullOrWhiteSpace(Name) ? "Guest" : Name);
+
+            return Page();
+        }
+    }
+}
