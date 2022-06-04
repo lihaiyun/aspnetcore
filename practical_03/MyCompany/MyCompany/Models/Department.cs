312e5d6028ad2d10245be314347e51bc393c8cba
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-06-04 14:03:42 +0800

    Remove ?

--- practical_03/MyCompany/MyCompany/Models/Department.cs
+++ practical_03/MyCompany/MyCompany/Models/Department.cs
@@ -5,10 +5,10 @@ namespace MyCompany.Models
     public class Department
     {
         [Required, MinLength(2), MaxLength(8)]
-        public string? Id { get; set; }
+        public string Id { get; set; }
 
         [Required, MaxLength(30)]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         public List<Employee> Employees { get; set; }
     }
--- practical_03/MyCompany/MyCompany/Models/Employee.cs
+++ practical_03/MyCompany/MyCompany/Models/Employee.cs
@@ -6,16 +6,16 @@ namespace MyCompany.Models
     {
         [Required, MinLength(3, ErrorMessage = "Enter at least 3 characters."), MaxLength(8)]
         [Display(Name = "Employee ID")]
-        public string? Id { get; set; }
+        public string Id { get; set; }
 
         [Required, RegularExpression(@"^[STFG]\d{7}[A-Z]$", ErrorMessage = "Invalid NRIC.")]
-        public string? NRIC { get; set; }
+        public string NRIC { get; set; }
 
         [Required, MaxLength(50)]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Required]
-        public string? Gender { get; set; }
+        public string Gender { get; set; }
 
         [DataType(DataType.Date)]
         [Display(Name = "Birthday")]
@@ -26,7 +26,7 @@ namespace MyCompany.Models
 
         [Required]
         [Display(Name = "Department")]
-        public string? DepartmentId { get; set; }
+        public string DepartmentId { get; set; }
 
         public Department Department { get; set; }
     }
