04d65954a0268cfee78cd46a455cd524703f8c8f
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-06-10 16:00:42 +0800

    Create dbo.Departments.data.sql

---
+++ practical_04/MyCompany/MyCompany/Migrations/dbo.Departments.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[Departments] ([DepartmentId], [Name]) VALUES (N'FIN', N'Finance')
+INSERT INTO [dbo].[Departments] ([DepartmentId], [Name]) VALUES (N'HR', N'Human Resource')
+INSERT INTO [dbo].[Departments] ([DepartmentId], [Name]) VALUES (N'IT', N'Information Technology')
+INSERT INTO [dbo].[Departments] ([DepartmentId], [Name]) VALUES (N'SAL', N'Sales')
