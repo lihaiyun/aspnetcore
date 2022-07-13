13abc421b85f6ed7ac3534ec0c28eca8a6f270ce
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-07-13 17:00:39 +0800

    Add-Migration InitialCreate

---
+++ practical_05/CarLoanAPI/CarLoanAPI/Migrations/20220713084813_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarLoanAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LoanApplications",
+                columns: table => new
+                {
+                    LoanApplicationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NRIC = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Contact = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(9,2)", nullable: false),
+                    LoanAmt = table.Column<decimal>(type: "decimal(12,2)", nullable: false),
+                    Term = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoanApplications", x => x.LoanApplicationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoanApplications");
+        }
+    }
+}
