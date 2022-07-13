97ec22dbf3efb7e38dffff0f7912adc897614b38
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-07-13 16:49:56 +0800

    Add db context with model

---
+++ practical_05/CarLoanAPI/CarLoanAPI/Models/LoanApplication.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarLoanAPI.Models
+{
+    public class LoanApplication
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid LoanApplicationId { get; set; }
+
+        [Required, RegularExpression(@"^[STFG]\d{7}[A-Z]$", ErrorMessage = "Invalid NRIC."), MaxLength(9)]
+        public string NRIC { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MinLength(8, ErrorMessage = "Invalid phone number."), MaxLength(8)]
+        public string Contact { get; set; } = string.Empty;
+
+        [Range(2000, 1e6)]
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Salary { get; set; }
+
+        [Range(5000, 1e9)]
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal LoanAmt { get; set; }
+
+        [Range(1, 10)]
+        public int Term { get; set; }
+    }
+}
---
+++ practical_05/CarLoanAPI/CarLoanAPI/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CarLoanAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarLoanAPI
+{
+    public class MyDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetConnectionString("MyConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<LoanApplication>? LoanApplications { get; set; }
+    }
+}
