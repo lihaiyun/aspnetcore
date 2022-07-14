0456cfd044c6ca2e56cab6171867d2992d5fa164
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-07-14 12:12:31 +0800

    Add models and service

---
+++ practical_06/BWW/BWW/Models/Instalment.cs
@@ -0,0 +1,13 @@
+﻿namespace BWW.Models
+{
+    public class Instalment
+    {
+        public decimal LoanAmt { get; set; }
+
+        public int Term { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public decimal InstalmentAmt { get; set; }
+    }
+}
