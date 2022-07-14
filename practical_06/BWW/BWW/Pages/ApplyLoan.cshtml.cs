b4446d8bdd61f1be4b4131bde85a1bce472eece2
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-07-14 13:49:07 +0800

    apply loan

---
+++ practical_06/BWW/BWW/Pages/ApplyLoan.cshtml.cs
@@ -0,0 +1,51 @@
+using BWW.Models;
+using BWW.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BWW.Pages
+{
+    public class ApplyLoanModel : PageModel
+    {
+        [BindProperty]
+        public LoanApplication MyApplication { get; set; } = new();
+
+        public string MyMsg { get; set; } = string.Empty;
+
+        private readonly LoanService _loanService;
+
+        public ApplyLoanModel(LoanService loanService)
+        {
+            _loanService = loanService;
+        }
+
+        public void OnGet(decimal loanAmt, int term)
+        {
+            MyApplication.LoanAmt = loanAmt;
+            MyApplication.Term = term;
+
+            MyApplication.Name = "Haiyun";
+            MyApplication.NRIC = "S1234567A";
+            MyApplication.Contact = "65501622";
+            MyApplication.Salary = 5000;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = _loanService.SubmitLoanApplication(MyApplication);
+                if (result)
+                {
+                    MyMsg = "Your loan application is submitted for bank processing!";
+                }
+                else
+                {
+                    MyMsg = "Your loan application is not successful, please correct input error.";
+                    MyMsg += " If problem persists, please contact system administrator!";
+                }
+            }
+            return Page();
+        }
+    }
+}
