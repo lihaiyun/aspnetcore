89ef1dec5687f04f63bd97dce57b6d882c94b692
Li Haiyun <li_haiyun@nyp.edu.sg>
2022-07-15 11:53:52 +0800

    create starter

---
+++ practical_06/BWW_Starter/BWW/Models/LoanApplication.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BWW.Models
+{
+    public class LoanApplication
+    {
+        [Required, RegularExpression(@"^[STFG]\d{7}[A-Z]$", ErrorMessage = "Invalid NRIC."), MaxLength(9)]
+        public string NRIC { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MinLength(8, ErrorMessage = "Invalid phone number."), MaxLength(8)]
+        public string Contact { get; set; } = string.Empty;
+
+        [Range(2000, 1e6)]
+        [Display(Name = "Monthly Salary")]
+        public decimal Salary { get; set; }
+
+        [Range(5000, 1e9)]
+        [Display(Name = "Loan Amount")]
+        public decimal LoanAmt { get; set; }
+
+        [Range(1, 7)]
+        public int Term { get; set; }
+    }
+}
---
+++ practical_06/BWW_Starter/BWW/Pages/CarLoan/Apply.cshtml.cs
@@ -0,0 +1,63 @@
+using BWW.Models;
+using BWW.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BWW.Pages.CarLoan
+{
+    public class ApplyModel : PageModel
+    {
+        [BindProperty]
+        public LoanApplication MyApplication { get; set; } = new();
+
+        private readonly LoanService _loanService;
+
+        public ApplyModel(LoanService loanService)
+        {
+            _loanService = loanService;
+        }
+
+        public void OnGet(decimal loanAmt, int term)
+        {
+            MyApplication.LoanAmt = loanAmt;
+            MyApplication.Term = term;
+
+            // Test
+            MyApplication.Name = "Haiyun";
+            MyApplication.NRIC = "S1234567A";
+            MyApplication.Contact = "98765432";
+            MyApplication.Salary = 5000;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    bool result = _loanService.SubmitLoanApplication(MyApplication);
+                    if (!result)
+                    {
+                        string msg = "Your loan application is not successful. Please try again later.";
+                        TempData["FlashMessage.Type"] = "danger";
+                        TempData["FlashMessage.Text"] = msg;
+                    }
+                    else
+                    {
+                        string msg = "Your loan application has been submitted for bank processing.";
+                        TempData["FlashMessage.Type"] = "success";
+                        TempData["FlashMessage.Text"] = msg;
+                        return Redirect("/CarLoan/Calculator");
+                    }
+                }
+                catch (Exception)
+                {
+                    string msg = "The service is not available. Please try again later.";
+                    TempData["FlashMessage.Type"] = "danger";
+                    TempData["FlashMessage.Text"] = msg;
+                }
+            }
+            return Page();
+        }
+    }
+}
